02c81f31faec2891d7dba9c0fe523640945e1961
Esteban Cruz <estebancruz1986@gmail.com>
2025-07-17 17:52:20 -0600

    Update telescope.lua
    Update layout

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,7 +1,16 @@
 require('telescope').setup({
+  defaults = {
+    sorting_strategy = "ascending",
+    layout_strategy = "horizontal",
+    layout_config = {
+      horizontal = {
+        prompt_position = "top",
+      },
+    },
+  },
   pickers = {
     find_files = {
       find_command = { "ag", "--silent", "--nocolor", "--follow", "-g", "", "--literal", "--hidden", "--ignore", ".git " },
-    }
+    },
   }
 })
